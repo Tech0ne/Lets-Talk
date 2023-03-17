5233b9a5cc137b1292acb9a5b5d6bb5a9aa8c9c3
Tech0ne <114503799+Tech0ne@users.noreply.github.com>
2023-03-17 13:06:06 +0100

    Update install_fedora.sh

--- install_fedora.sh
+++ install_fedora.sh
@@ -5,6 +5,7 @@ echo "Press enter to accept that you use an OS that is inferior :eyes:"
 read
 
 sudo dnf install -y python3 python3-pip
+sudo yum install -y python3-netifaces
 sudo pip3 install -r requirements.txt
 
 sudo cp code/main.py /usr/bin/local_chat
